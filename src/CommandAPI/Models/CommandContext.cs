4e93fa0aa390d57df7c2a3155ec4cd3f5ae8cc1b
Zoltan Halasz <42749923+zoltanhalasz@users.noreply.github.com>
2020-05-01 11:14:23 +0300

    added database and context, including migrations

---
+++ src/CommandAPI/Models/CommandContext.cs
@@ -0,0 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+namespace CommandAPI.Models
+{
+    public class CommandContext : DbContext
+    {
+        public CommandContext(DbContextOptions<CommandContext> options) : base(options) { }
+        public DbSet<Command> CommandItems {get; set;}
+    }
+}
